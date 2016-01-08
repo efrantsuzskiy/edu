3bca33ed626699867621651dfa8ca41b8fef95b4
E <e.frantsuzskiy@gmail.com>
2016-01-08 23:09:44 +0300

    Initial Commit

---
+++ edu/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  edu
+//
+//  Created by Евгений Французский on 08.01.16.
+//  Copyright © 2016 Евгений Французский. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
